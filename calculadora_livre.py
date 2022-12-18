34de4feeabbe94333dbfe0b31c943838095939dc
Mirelle Mota <mirelle.motta06@gmail.com>
2022-12-18 11:38:13 -0300

    Exercícios de Python

---
+++ calculadora_livre.py
@@ -0,0 +1,48 @@
+def calculate():
+    operation = input('''
+Digite qual operação deseja realizar:
++ para adicão
+- para subtração
+* para multiplicação
+/ para divisão
+''')
+
+    number_1 = int(input('Por favor, entre com o primeiro número: '))
+    number_2 = int(input('Por favor entre com o segundo número: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('Você não digitou um operador válido, por favor, digite novamente.')
+
+    # Add again() function to calculate() function
+    again()
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
